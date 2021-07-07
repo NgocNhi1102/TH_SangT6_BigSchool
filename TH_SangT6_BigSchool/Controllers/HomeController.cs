1a0c39873af2d4f6705a9decae5f933537c7d843
Ngọc Nhi <ngocnhi12b1thd@gmail.com>
2021-07-07 15:37:05 +0700

    Up 6.3 6.4 6.5 6.6

--- TH_SangT6_BigSchool/Controllers/HomeController.cs
+++ TH_SangT6_BigSchool/Controllers/HomeController.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TH_SangT6_BigSchool.Models;
 
 namespace TH_SangT6_BigSchool.Controllers
 {
     public class HomeController : Controller
     {
+        //private ApplicationDbContext _dbContext;
+
+        /*public HomeController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }*/
+
         public ActionResult Index()
         {
-            return View();
+            
+            /*var upcommingCourses = _dbContext.Courses
+                .Include(c => c.Lecturer)
+                .Include( c => c.Category)
+                .Where(c => c.DateTime > DateTime.Now);
+            */
+            return View(/*upcommingCourses*/);
         }
 
         public ActionResult About()
--- TH_SangT6_BigSchool/Models/AccountViewModels.cs
+++ TH_SangT6_BigSchool/Models/AccountViewModels.cs
@@ -79,6 +79,10 @@ namespace TH_SangT6_BigSchool.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
     }
 
     public class ResetPasswordViewModel
